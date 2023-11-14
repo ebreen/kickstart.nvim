e95cf98ed342705001ea6addc622d49653cc50b0
ebreen <eirik.breen@basefarm-orange.com>
2023-11-14 13:58:57 +0100

    added startify alternative, alphavim plugin

---
+++ lua/custom/plugins/alphavim.lua
@@ -0,0 +1,7 @@
+{
+    'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.startify'.config)
+    end
+};
