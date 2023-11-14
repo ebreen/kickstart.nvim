da34af867ac6d2d961a7bde3b5c281be5d61b9cc
ebreen <eirik.breen@basefarm-orange.com>
2023-11-14 14:10:42 +0100

    fix

--- lua/custom/plugins/alphavim.lua
+++ lua/custom/plugins/alphavim.lua
@@ -1,7 +1,9 @@
-{
-    'goolord/alpha-nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
-};
+return {
+    {
+        'goolord/alpha-nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function ()
+            require'alpha'.setup(require'alpha.themes.startify'.config)
+        end
+    }
+}
